5f802125478c475bb436cd1b82f14a1cf5c0ad64
Pixxilated <LeslieMCunningham@outlook.com>
2025-04-17 15:29:08 -0400

    FINISHED Import methods ! Switched importing the files information to the database to another method on its own so that it can be used by both importing a file and a folder.

--- CSC 440 Group Project/ImportRecords.Designer.cs
+++ CSC 440 Group Project/ImportRecords.Designer.cs
@@ -39,10 +39,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Yu Gothic UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(135, 142);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(91, 92);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 35);
+            this.button1.Size = new System.Drawing.Size(69, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "From File";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,10 +51,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Yu Gothic UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(352, 142);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(235, 92);
+            this.button2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(104, 35);
+            this.button2.Size = new System.Drawing.Size(88, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "From Folder";
             this.button2.UseVisualStyleBackColor = true;
@@ -65,10 +65,10 @@
             this.mainLabel.AutoSize = true;
             this.mainLabel.Font = new System.Drawing.Font("Yu Gothic UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.mainLabel.ForeColor = System.Drawing.Color.White;
-            this.mainLabel.Location = new System.Drawing.Point(124, 45);
-            this.mainLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.mainLabel.Location = new System.Drawing.Point(83, 29);
+            this.mainLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.mainLabel.Name = "mainLabel";
-            this.mainLabel.Size = new System.Drawing.Size(353, 65);
+            this.mainLabel.Size = new System.Drawing.Size(240, 45);
             this.mainLabel.TabIndex = 2;
             this.mainLabel.Text = "Import Records";
             this.mainLabel.Click += new System.EventHandler(this.mainLabel_Click);
@@ -80,10 +80,10 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Yu Gothic UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(243, 242);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(162, 157);
+            this.button3.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 35);
+            this.button3.Size = new System.Drawing.Size(63, 23);
             this.button3.TabIndex = 3;
             this.button3.Text = "Cancel";
             this.button3.UseVisualStyleBackColor = true;
@@ -91,15 +91,15 @@
             // 
             // importRecords
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.ClientSize = new System.Drawing.Size(400, 234);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.mainLabel);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Name = "importRecords";
             this.Text = "Import Records";
             this.ResumeLayout(false);
--- CSC 440 Group Project/ImportRecords.cs
+++ CSC 440 Group Project/ImportRecords.cs
@@ -22,17 +22,62 @@ namespace CSC_440_Group_Project
 
         private void button2_Click(object sender, EventArgs e)
         {
-            DialogResult result = folderBrowserDialog1.ShowDialog();
-            if (result == DialogResult.OK)
+            // Allow the user to select a folder from their desktop
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
             {
-                /**
-                 * Need to add code that validates the folder name and imports all excel files in the folder
-                 */
+                DialogResult result = folderDialog.ShowDialog();
 
-                MessageBox.Show("Grades have been successfully imported.", "Import Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+                {
+                    // Variables - Store the folders path and the files inside the folder
+                    string folderPath = folderDialog.SelectedPath;
+                    string[] files = Directory.GetFiles(folderPath);
+
+                    // Parse through the folder to select each file
+                    foreach (string file in files)
+                    {
+                        // Variables - Store the file name and the file extension how many rows it contains (for checking usability) and the file name for each column
+                        string filenameString = Path.GetFileNameWithoutExtension(file);
+                        string fileExtension = Path.GetExtension(file);
+                        string[] filename = filenameString.Split(' ');
+                        string[] fileRows = File.ReadAllLines(file);
+
+                        // Check if the file we are currently looking at is in .csv format
+                        if (fileExtension != ".csv" && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+                        {
+                            // File was not in .csv format - Output error message and send user back to the import screen
+                            MessageBox.Show("Invalid file type. Grades must be imported from an Excel file (.csv).", "Import Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                        else
+                        {
+                            // File was in .csv format - Check if the file name is valid
+                            // Following the format, [0] will be the prefix, [1] is the number, [2] is the year, [3] is the semester
+                            if (filename.Length != 4 || filename[0].Length != 3 || filename[1].Length != 3 || filename[2].Length != 4)
+                            {
+                                // File was not in the correct format - Output error message and send user back to the import screen
+                                // Show the user which file name is incorrect
+                                MessageBox.Show("Invalid file name." + filenameString + " File name must follow the format [Course Prefix] [Course Numer] [Year] [Semester].", "Import Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+
+                            // Call Import Records Here
+                            ImportRecordsFromFile(fileRows, filename);
+                        }
+                    }
+                    // Show the user that the import was successful
+                    MessageBox.Show("Grades have been successfully imported.", "Import Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    // Do nothing
+                }
+
+                // Close application
                 this.Close();
             }
         }
+
         /*
          *      Steps for Importing a Record
          *      1. Gather if the record is in a file or a folder.
@@ -58,24 +103,6 @@ namespace CSC_440_Group_Project
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Create Attributes
-            // These are taken from the file name
-            string coursePrefix;
-            string courseNum;
-            string year;
-            string semester;
-
-            // These are taken from the file lines
-            string grade;
-            string studentID;
-            string studentName;
-
-            // Connect to the database
-            string connStr = "server=csitmariadb.eku.edu;user=student;database=csc340_db;port=3306;password=Maroon@21?;";
-            using (MySqlConnection conn = new MySqlConnection(connStr))
-            {
-                conn.Open();
-
                 // Only allow files with .csv
                 openFileDialog1.Filter = "CSV files (*.csv)|*.csv";
 
@@ -101,88 +128,15 @@ namespace CSC_440_Group_Project
                         return;
                     }
 
-                    // Extract the file name to their components
-                    coursePrefix = filename[0];
-                    courseNum = filename[1];
-                    year = filename[2];
-                    semester = filename[3];
-
-                    // Prep the sql statement for grades
-                    string sqlGrades = "INSERT INTO sklc440grades (studentID, coursePrefix, courseNum, grade, year, semester) SELECT @studentID, @coursePrefix, @courseNum, @grade, @year, @semester WHERE NOT EXISTS (SELECT 1 FROM sklc440grades WHERE studentID = @studentID AND coursePrefix = @coursePrefix AND courseNum = @courseNum AND year = @year AND semester = @semester);";
-                    MySqlCommand cmdG = new MySqlCommand(sqlGrades, conn);
-
-                    // Prep sql statement for student
-                    string sqlStudent = "INSERT IGNORE INTO sklc440student (studentID, studentName) VALUES (@studentID, @studentName)";
-                    MySqlCommand cmdS = new MySqlCommand(sqlStudent, conn);
-
-                    // Initialize how many cols are present for each student/row
-                    // For our case it is 3: Student Name, Student ID, and Grade
-                    int colsPerStudent = 3;
-
-                    // Start at i=1 since i=0 will be the headers
-                    for (int student = 1; student < fileRows.Length; student++)
-                    {
-                        Console.WriteLine(fileRows[student]);
-                        //Split the current student line into columns
-                        string[] columns = fileRows[student].Split(',');
-
-                        //Ensure the student we grabbed has all 3 columns needed
-                        if (columns.Length < colsPerStudent)
-                        {
-                            MessageBox.Show($"Invalid data on line {student + 1}.", "Import Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                            continue;
-                        }
-
-                        // Store each column to its own variable
-                        studentName = columns[0].Trim();
-                        studentID = columns[1].Trim();
-                        grade = columns[2].Trim();
-
-                        // Check if there is already a grade for that student for that class
-
-                        // Add to grades table
-                        cmdG.Parameters.AddWithValue("@studentID", studentID);
-                        cmdG.Parameters.AddWithValue("@coursePrefix", coursePrefix);
-                        cmdG.Parameters.AddWithValue("@courseNum", courseNum);
-                        cmdG.Parameters.AddWithValue("@grade", grade);
-                        cmdG.Parameters.AddWithValue("year", year);
-                        cmdG.Parameters.AddWithValue("semester", semester);
-
-                        // Check if there is already a student for that ID
-
-                        // Add to student table
-                        cmdS.Parameters.AddWithValue("@studentID", studentID);
-                        cmdS.Parameters.AddWithValue("@studentName", studentName);
-
-                        //Execute Queries
-                        cmdG.ExecuteNonQuery();
-                        cmdS.ExecuteNonQuery();
-
-                        // clear parameters for another student
-                        cmdG.Parameters.Clear();
-                        cmdS.Parameters.Clear();
-                    }
-
-                    // Prep sql select statement for course
-                    string sqlCourse = "INSERT INTO sklc440courses (coursePrefix, courseNum, year, semester) SELECT @coursePrefix, @courseNum, @year, @semester WHERE NOT EXISTS (SELECT 1 FROM sklc440courses WHERE coursePrefix = @coursePrefix AND courseNum = @courseNum AND year = @year AND semester = @semester);";
-
-                    using (MySqlCommand cmdC = new MySqlCommand(sqlCourse, conn))
-                    {
-                        // Add to course table
-                        cmdC.Parameters.AddWithValue("@coursePrefix", coursePrefix);
-                        cmdC.Parameters.AddWithValue("@courseNum", courseNum);
-                        cmdC.Parameters.AddWithValue("@year", year);
-                        cmdC.Parameters.AddWithValue("@semester", semester);
-
-                        cmdC.ExecuteNonQuery();
-                    }
-                }
+                // Call Import Records Here
+                ImportRecordsFromFile(fileRows, filename);
+                MessageBox.Show("Grades have been successfully imported.", "Import Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
                 else if (result == DialogResult.Cancel)
                 {
 
                 }
                 this.Close();
-            }
         }
 
         private void mainLabel_Click(object sender, EventArgs e)
@@ -193,6 +147,105 @@ namespace CSC_440_Group_Project
         private void button3_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        // The ImportRecordsFromFile method takes the file name and the rows of the file and imports the records into the database.
+        private void ImportRecordsFromFile(String[] fileRows, String[] filename)
+        {
+            // Create Attributes
+            // These are taken from the file name
+            string coursePrefix;
+            string courseNum;
+            string year;
+            string semester;
+
+            // These are taken from the file lines
+            string grade;
+            string studentID;
+            string studentName;
+
+            // Connect to the database
+            string connStr = "server=csitmariadb.eku.edu;user=student;database=csc340_db;port=3306;password=Maroon@21?;";
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                // Extract the file name to their components
+                coursePrefix = filename[0];
+                courseNum = filename[1];
+                year = filename[2];
+                semester = filename[3];
+
+                // Prep the sql statement for grades
+                string sqlGrades = "INSERT INTO sklc440grades (studentID, coursePrefix, courseNum, grade, year, semester) SELECT @studentID, @coursePrefix, @courseNum, @grade, @year, @semester WHERE NOT EXISTS (SELECT 1 FROM sklc440grades WHERE studentID = @studentID AND coursePrefix = @coursePrefix AND courseNum = @courseNum AND year = @year AND semester = @semester);";
+                MySqlCommand cmdG = new MySqlCommand(sqlGrades, conn);
+
+                // Prep sql statement for student
+                string sqlStudent = "INSERT IGNORE INTO sklc440student (studentID, studentName) VALUES (@studentID, @studentName)";
+                MySqlCommand cmdS = new MySqlCommand(sqlStudent, conn);
+
+                // Initialize how many cols are present for each student/row
+                // For our case it is 3: Student Name, Student ID, and Grade
+                int colsPerStudent = 3;
+
+                // Start at i=1 since i=0 will be the headers
+                for (int student = 1; student < fileRows.Length; student++)
+                {
+                    Console.WriteLine(fileRows[student]);
+                    //Split the current student line into columns
+                    string[] columns = fileRows[student].Split(',');
+
+                    //Ensure the student we grabbed has all 3 columns needed
+                    if (columns.Length < colsPerStudent)
+                    {
+                        MessageBox.Show($"Invalid data on line {student + 1}. {columns.Length}", "Import Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        continue;
+                    }
+
+                    // Store each column to its own variable
+                    studentName = columns[0].Trim();
+                    studentID = columns[1].Trim();
+                    grade = columns[2].Trim();
+
+                    // Check if there is already a grade for that student for that class
+
+                    // Add to grades table
+                    cmdG.Parameters.AddWithValue("@studentID", studentID);
+                    cmdG.Parameters.AddWithValue("@coursePrefix", coursePrefix);
+                    cmdG.Parameters.AddWithValue("@courseNum", courseNum);
+                    cmdG.Parameters.AddWithValue("@grade", grade);
+                    cmdG.Parameters.AddWithValue("year", year);
+                    cmdG.Parameters.AddWithValue("semester", semester);
+
+                    // Check if there is already a student for that ID
+
+                    // Add to student table
+                    cmdS.Parameters.AddWithValue("@studentID", studentID);
+                    cmdS.Parameters.AddWithValue("@studentName", studentName);
+
+                    //Execute Queries
+                    cmdG.ExecuteNonQuery();
+                    cmdS.ExecuteNonQuery();
+
+                    // clear parameters for another student
+                    cmdG.Parameters.Clear();
+                    cmdS.Parameters.Clear();
+                }
+
+                // Prep sql select statement for course
+                string sqlCourse = "INSERT INTO sklc440courses (coursePrefix, courseNum, year, semester) SELECT @coursePrefix, @courseNum, @year, @semester WHERE NOT EXISTS (SELECT 1 FROM sklc440courses WHERE coursePrefix = @coursePrefix AND courseNum = @courseNum AND year = @year AND semester = @semester);";
+
+                using (MySqlCommand cmdC = new MySqlCommand(sqlCourse, conn))
+                {
+                    // Add to course table
+                    cmdC.Parameters.AddWithValue("@coursePrefix", coursePrefix);
+                    cmdC.Parameters.AddWithValue("@courseNum", courseNum);
+                    cmdC.Parameters.AddWithValue("@year", year);
+                    cmdC.Parameters.AddWithValue("@semester", semester);
+
+                    cmdC.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
