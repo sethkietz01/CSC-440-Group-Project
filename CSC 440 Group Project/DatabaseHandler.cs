a53b8abef6316611811fafb3197f76b4f49c9bb7
Seth Kietz <skietz01@gmail.com>
2025-04-24 22:26:51 -0400

    Changed Import Records to Object-Oriented Design

--- CSC 440 Group Project/DatabaseHandler.cs
+++ CSC 440 Group Project/DatabaseHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,195 @@ namespace CSC_440_Group_Project
 {
     internal class DatabaseHandler
     {
+        public void ImportRecordsFolder()
+        {
+            // Allow the user to select a folder from their desktop
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+                {
+                    // Variables - Store the folders path and the files inside the folder
+                    string folderPath = folderDialog.SelectedPath;
+                    string[] files = Directory.GetFiles(folderPath);
+
+                    // Parse through the folder to select each file
+                    foreach (string file in files)
+                    {
+                        // Variables - Store the file name and the file extension how many rows it contains (for checking usability) and the file name for each column
+                        string filenameString = Path.GetFileNameWithoutExtension(file);
+                        string fileExtension = Path.GetExtension(file);
+                        string[] filename = filenameString.Split(' ');
+                        string[] fileRows = File.ReadAllLines(file);
+
+                        // Check if the file we are currently looking at is in .csv format
+                        if (fileExtension != ".csv" && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+                        {
+                            // File was not in .csv format - Output error message and send user back to the import screen
+                            MessageBox.Show("Invalid file type. Grades must be imported from an Excel file (.csv).", "Import Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                        else
+                        {
+                            // File was in .csv format - Check if the file name is valid
+                            // Following the format, [0] will be the prefix, [1] is the number, [2] is the year, [3] is the semester
+                            if (filename.Length != 4 || filename[0].Length != 3 || filename[1].Length != 3 || filename[2].Length != 4)
+                            {
+                                // File was not in the correct format - Output error message and send user back to the import screen
+                                // Show the user which file name is incorrect
+                                MessageBox.Show("Invalid file name." + filenameString + " File name must follow the format [Course Prefix] [Course Numer] [Year] [Semester].", "Import Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+
+                            // Call Import Records Here
+                            ImportRecordsFromFile(fileRows, filename);
+                        }
+                    }
+                    // Show the user that the import was successful
+                    MessageBox.Show("Grades have been successfully imported.", "Import Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    // Do nothing
+                }
+
+            }
+        }
+
+        // The ImportRecordsFromFile method takes the file name and the rows of the file and imports the records into the database.
+        public void ImportRecordsFromFile(String[] fileRows, String[] filename)
+        {
+            // Create Attributes
+            // These are taken from the file name
+            string coursePrefix;
+            string courseNum;
+            string year;
+            string semester;
+
+            // These are taken from the file lines
+            string grade;
+            string studentID;
+            string studentName;
+
+            // Connect to the database
+            string connStr = "server=csitmariadb.eku.edu;user=student;database=csc340_db;port=3306;password=Maroon@21?;";
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                // Extract the file name to their components
+                coursePrefix = filename[0];
+                courseNum = filename[1];
+                year = filename[2];
+                semester = filename[3];
+
+
+
+                // Prep the sql statement for grades
+                string sqlGrades = "INSERT INTO sklc440grades (studentID, coursePrefix, courseNum, grade, year, semester) SELECT @studentID, @coursePrefix, @courseNum, @grade, @year, @semester WHERE NOT EXISTS (SELECT 1 FROM sklc440grades WHERE studentID = @studentID AND coursePrefix = @coursePrefix AND courseNum = @courseNum AND year = @year AND semester = @semester);";
+                MySqlCommand cmdG = new MySqlCommand(sqlGrades, conn);
+
+                // Prep sql statement for student
+                string sqlStudent = "INSERT IGNORE INTO sklc440student (studentID, studentName) VALUES (@studentID, @studentName)";
+                MySqlCommand cmdS = new MySqlCommand(sqlStudent, conn);
+
+                // Initialize how many cols are present for each student/row
+                // For our case it is 3: Student Name, Student ID, and Grade
+                int colsPerStudent = 3;
+
+                // Start at i=1 since i=0 will be the headers
+                for (int student = 1; student < fileRows.Length; student++)
+                {
+                    Console.WriteLine(fileRows[student]);
+                    //Split the current student line into columns
+                    string[] columns = fileRows[student].Split(',');
+
+                    //Ensure the student we grabbed has all 3 columns needed
+                    if (columns.Length < colsPerStudent)
+                    {
+                        MessageBox.Show($"Invalid data on line {student + 1}. {columns.Length}", "Import Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        continue;
+                    }
+
+                    // Store each column to its own variable
+                    studentName = columns[0].Trim();
+                    studentID = columns[1].Trim();
+                    grade = columns[2].Trim();
+
+                    // Add to grades table
+                    cmdG.Parameters.AddWithValue("@studentID", studentID);
+                    cmdG.Parameters.AddWithValue("@coursePrefix", coursePrefix);
+                    cmdG.Parameters.AddWithValue("@courseNum", courseNum);
+                    cmdG.Parameters.AddWithValue("@grade", grade);
+                    cmdG.Parameters.AddWithValue("year", year);
+                    cmdG.Parameters.AddWithValue("semester", semester);
+
+                    // Check if there is already a student for that ID
+
+                    // Add to student table
+                    cmdS.Parameters.AddWithValue("@studentID", studentID);
+                    cmdS.Parameters.AddWithValue("@studentName", studentName);
+
+                    //Execute Queries
+                    cmdG.ExecuteNonQuery();
+                    cmdS.ExecuteNonQuery();
+
+                    // clear parameters for another student
+                    cmdG.Parameters.Clear();
+                    cmdS.Parameters.Clear();
+                }
+
+                // Prep sql select statement for course
+                string sqlCourse = "INSERT INTO sklc440courses (coursePrefix, courseNum, year, semester, hours) SELECT @coursePrefix, @courseNum, @year, @semester, @hours WHERE NOT EXISTS (SELECT 1 FROM sklc440courses WHERE coursePrefix = @coursePrefix AND courseNum = @courseNum AND year = @year AND semester = @semester AND hours = @hours);";
+
+                string hours = InputHours();
+
+                using (MySqlCommand cmdC = new MySqlCommand(sqlCourse, conn))
+                {
+                    // Add to course table
+                    cmdC.Parameters.AddWithValue("@coursePrefix", coursePrefix);
+                    cmdC.Parameters.AddWithValue("@courseNum", courseNum);
+                    cmdC.Parameters.AddWithValue("@year", year);
+                    cmdC.Parameters.AddWithValue("@semester", semester);
+                    cmdC.Parameters.AddWithValue("@hours", hours);
+
+                    cmdC.ExecuteNonQuery();
+
+                }
+            }
+        }
+
+        // Input hours method is only called when there is a new course added to the database that was not there previously
+        // It does not take any parameters and returns a string of the course hours.
+        // Its a small popup.
+        private string InputHours()
+        {
+            // New course was added - Need to gather the hours for the course
+            Form hoursPromptForm = new Form()
+            {
+                Width = 350,
+                Height = 200,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = "Course Hours",
+                StartPosition = FormStartPosition.CenterScreen,
+                MinimizeBox = false,
+                MaximizeBox = false
+            };
+
+            Label inputHoursLabel = new Label() { Left = 10, Top = 10, Text = "Enter the number of hours for the course:", AutoSize = true };
+            System.Windows.Forms.TextBox inputHoursTextBox = new System.Windows.Forms.TextBox() { Left = 10, Top = 35, Width = 300 };
+
+            System.Windows.Forms.Button confirmation = new System.Windows.Forms.Button() { Text = "OK", Left = 220, Width = 90, Top = 70, DialogResult = DialogResult.OK };
+            hoursPromptForm.AcceptButton = confirmation;
+
+            hoursPromptForm.Controls.Add(inputHoursLabel);
+            hoursPromptForm.Controls.Add(inputHoursTextBox);
+            hoursPromptForm.Controls.Add(confirmation);
+
+            DialogResult result = hoursPromptForm.ShowDialog();
+            return result == DialogResult.OK ? inputHoursTextBox.Text : null;
+        }
 
         public void addRecord(string studentID, string coursePrefix, string courseNum, string grade, string year, string semester)
         {
